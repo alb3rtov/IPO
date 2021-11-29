e4b74c40ce8ed1f6b1e4c482e6f3602122120cfc
Alberto Vazquez <albertovmrm@gmail.com>
2021-11-29 11:08:44 +0100

    Login and new management window

--- Protectora/MainWindow.xaml.cs
+++ Protectora/MainWindow.xaml.cs
@@ -20,14 +20,65 @@ namespace Protectora
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string usuario = "admin";
+        private string password = "ipo1";
+        private Window management;
+
         public MainWindow()
         {
             InitializeComponent();
+            txtUsuario.Focus();
         }
+
+        private void checkCredentials() {
+            if (txtUsuario.Text == usuario && txtPassword.Text == password)
+            {
+                management = new Management();
+                this.Close();
+                management.Show();
+
+            }
+            else if (txtUsuario.Text == "" || txtPassword.Text == "")
+            {
+                MessageBox.Show("Debe introducir alguna cadena en los campos de usuario y constraseña", "Credenciales incorrectas", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtUsuario.Text = "";
+                txtPassword.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("El usuario o la contraseña no es correcto", "Credenciales incorrectas", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtUsuario.Text = "";
+                txtPassword.Text = "";
+            }
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            checkCredentials();
+        }
 
+        private void Button_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return) {
+                checkCredentials();
+            }
+        }
+
+        private void txtUsuario_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                checkCredentials();
+            }
+        }
+
+        private void txtPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                checkCredentials();
+            }
         }
     }
 }
