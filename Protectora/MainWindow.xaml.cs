fce7f33818a72c0e41e97d5fb923191892bdd580
Alberto Vazquez <albertovmrm@gmail.com>
2021-12-04 15:11:34 +0100

    Show asterisk when enter password

--- Protectora/MainWindow.xaml.cs
+++ Protectora/MainWindow.xaml.cs
@@ -31,30 +31,56 @@ namespace Protectora
         }
 
         private void checkCredentials() {
-            if (txtUser.Text == user && txtPassword.Text == password)
+            if (txtUser.Text == user && txtPassword.Password == password)
             {
                 management = new Management(user);
                 this.Close();
                 management.Show();
 
             }
-            else if (txtUser.Text == "" || txtPassword.Text == "")
+            else if (txtUser.Text == "" || txtPassword.Password == "")
             {
                 MessageBox.Show("Debe introducir alguna cadena en los campos de usuario y constraseña", "Credenciales incorrectas", MessageBoxButton.OK, MessageBoxImage.Error);   
                 txtUser.Text = "";
-                txtPassword.Text = "";
+                txtPassword.Password = "";
             }
             else
             {
                 MessageBox.Show("El usuario o la contraseña no es correcto", "Credenciales incorrectas", MessageBoxButton.OK, MessageBoxImage.Error);
                 txtUser.Text = "";
-                txtPassword.Text = "";
+                txtPassword.Password = "";
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             checkCredentials();
+        }
+
+        private void Label_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("HOLA");
+        }
+
+        private void WrapPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            lblPassword.Visibility = Visibility.Hidden;
+            txtPassword.Focus();
+
+            //System.Diagnostics.Debug.WriteLine("hOLA");
+        }
+
+        private void txtPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            lblPassword.Visibility = Visibility.Hidden;
+        }
+
+        private void txtPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password.Length == 0)
+            {
+                lblPassword.Visibility = Visibility.Visible;
+            }
         }
     }
 }
